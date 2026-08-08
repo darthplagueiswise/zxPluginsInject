ff245cdc1fa9267edc6f0fd07af27e422d268282
euoradan <zelihmannradan@gmail.com>
2026-08-08 15:27:19 -0300

    Expose complete App Group alias mapping

--- src/Header.h
+++ src/Header.h
@@ -4,13 +4,16 @@ extern NSString *accessGroupId;
 extern NSString *bundleId;
 
 extern void rebindSecFuncs();
-extern void rebindPathFuncs();
 
-extern BOOL createDirectoryIfNotExists(NSString *path);
+extern void initializeAppGroupMapping(void);
 extern NSArray<NSString *> *signedApplicationGroups(void);
 extern NSURL *preferredRealAppGroupURL(void);
+extern NSString *preferredRealAppGroupIdentifier(void);
 extern NSURL *sideloadFallbackAppGroupURL(void);
 extern NSURL *getAppGroupPathIfExists(void);
+extern BOOL isMetaAppGroupIdentifier(NSString *groupIdentifier);
+extern NSDictionary *mappedApplicationGroupEntitlements(NSDictionary *entitlements);
+extern NSDictionary *mappedGroupContainerURLs(NSDictionary *groupContainerURLs);
 
 @interface LSBundleProxy: NSObject
 @property(nonatomic, assign, readonly) NSDictionary *entitlements;
