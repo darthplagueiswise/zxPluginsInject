fd4033871968519a75a8665cf3be258292dde026
euoradan <zelihmannradan@gmail.com>
2026-08-08 01:41:56 -0300

    Expose deterministic AppGroup helpers

--- src/Header.h
+++ src/Header.h
@@ -4,9 +4,13 @@ extern NSString *accessGroupId;
 extern NSString *bundleId;
 
 extern void rebindSecFuncs();
+extern void rebindPathFuncs();
 
 extern BOOL createDirectoryIfNotExists(NSString *path);
 extern NSURL *getAppGroupPathIfExists();
+extern NSString *canonicalizedSideloadPath(NSString *path);
+extern NSURL *canonicalizedSideloadURL(NSURL *url);
+extern void migrateLegacyMobileConfigIfNeeded();
 
 @interface LSBundleProxy: NSObject
 @property(nonatomic, assign, readonly) NSDictionary *entitlements;
